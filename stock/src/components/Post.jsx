ed962d3d1f0ac28a99967b44d8499d4b5838ecc5
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-08 15:24:59 -0300

    (AuthPags): learning about dynamic values

--- stock/src/components/Post.jsx
+++ stock/src/components/Post.jsx
@@ -1,10 +1,13 @@
 
+const names = ["Guilherme", "Guilherme Cosme dos Santos" ]
 
 export default function Post() {
 
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <>
-    <p>Guilherme</p>
+    <p> {chosenName} </p>
     <p>ReactJs is Awesome!</p>
     </>
   )
