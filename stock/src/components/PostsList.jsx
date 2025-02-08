8c7b657af910e9b97a6f93dc623a865124874a68
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-08 17:31:08 -0300

    (AuthPags: Adding State & Conditional's Content

--- stock/src/components/PostsList.jsx
+++ stock/src/components/PostsList.jsx
@@ -8,9 +8,13 @@ import Modal from "./Modal";
 
 export default function () {
 
-
+  const [ modalIsVisible, setModalIsVisible ] = useState(true);
   const [ enteredBody, setEnteredBody ] = useState("");
   const [ enteredAuthor, setEnteredAuthor ] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value); // Variável atualizada aqui
@@ -21,12 +25,35 @@ export default function () {
     setEnteredAuthor(event.target.value);
   }
 
+  //  2 - Abordagem
+  // let modalContent; 
+
+  // if (modalIsVisible) {
+  //   modalContent =  <Modal onClose={hideModalHandler} >
+  //   <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+  //   </Modal>
+  // }
+
 
   return (
     <>
-    <Modal>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+    {/* {modalContent} */}
+    {/* Maneir de introduzir o conteúdo condicionalmente */}
+
+    {/* 1 - { modalIsVisible ? ( 
+     <Modal onClose={hideModalHandler} >
+     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
     </Modal>
+    )  : false
+    } */}
+
+    {/* 3 - Abordagem */}
+    {modalIsVisible && (
+       <Modal onClose={hideModalHandler} >
+       <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      </Modal>
+    )}
+    
 
     <ul className={classes.posts}>
     <Post author={enteredAuthor} body={enteredBody}  />  
