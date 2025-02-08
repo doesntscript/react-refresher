65555516da74983b2aa674410c924b9c87c1c3ec
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-08 16:33:26 -0300

    (AuthPags): Working with State..

--- stock/src/components/NewPost.jsx
+++ stock/src/components/NewPost.jsx
@@ -1,10 +1,17 @@
 
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+    // const stateData = useState("");
+    // stateData[0] // current value , valor do estado atual. 
+    // stateData[1] // state updatin' fucntion , valor de atualização de estado(função). 
+
+    const [ enteredBody, setEnteredBody ] = useState('');
+    // let enteredBody = ''; // Variável e atualizada aqui. 
 
     function changeBodyHandler(event) {
-        console.log(event.target.value);
+        setEnteredBody(event.target.value); // Variável atualizada aqui
     
     }
 
@@ -15,6 +22,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p> {enteredBody} </p>  {/* emitida aqui  */}
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
