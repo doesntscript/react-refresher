68f5e7653039114de731210f8aef1622e07a23c5
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-11 07:57:15 -0300

    (AuthPags): Dynamic Routes updates

--- stock/src/components/Post.jsx
+++ stock/src/components/Post.jsx
@@ -1,16 +1,19 @@
 
 // const names = ["Guilherme", "Guilherme Cosme dos Santos" ];
 
+import { Link } from 'react-router-dom';
 import classes from './Post.module.css';
 
-export default function Post({ author, body }) {
+export default function Post({ id, author, body }) {
 
     // const chosenName = Math.random() > 0.5 ? names[0] : names[1];
 
   return (
     <li className={classes.post} >
-    <p className={classes.author} > {author} </p>
-    <p className={classes.text} > {body} </p>
+     <Link to={id}>
+     <p className={classes.author} > {author} </p>
+     <p className={classes.text} > {body} </p>   
+     </Link>
     </li>
   )
 }
--- stock/src/main.jsx
+++ stock/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Posts, { loader as postsLoader } from './routes/Posts';
-
 import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails , { loader as postDetailsLoader} from './routes/PostDetails';
+
 import RouterLayout from './routes/RouterLayout';
 import './index.css'
 
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
         element: <Posts />,
        loader: postsLoader, 
         children: [{ path: '/create-post', element: <NewPost/>, action: newPostAction },
+        { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader },  
         ],
       },  
     ],
